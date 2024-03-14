15132b468ea96df170f3f42e10a7b78b55e46a62
leiocopio <louiseangeloocopio@gmail.com>
2024-03-14 23:36:33 +0800

    Implement vercel

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,38 @@
-
 //import modules
-const express = require('express')
-const mongoose = require('mongoose')
-const userRoutes = require('./routes/user')
-require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/user");
+require("dotenv").config();
 
 // express app
-const app = express()
+const app = express();
 
 // middleware
-app.use(express.json())
+app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
-})
+  console.log(req.path, req.method);
+  next();
+});
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
 // routes
-app.use('/api/user', userRoutes)
+app.use("/api/user", userRoutes);
 
 // connect to db
-mongoose.connect(process.env.MONG_URI)
+mongoose
+  .connect(process.env.MONG_URI)
   .then(() => {
     // listen for requests
     app.listen(process.env.PORT, () => {
-      console.log('connected to db & listening on port', process.env.PORT)
-    })
+      console.log("connected to db & listening on port", process.env.PORT);
+    });
   })
   .catch((error) => {
-    console.log(error)
-  })
+    console.log(error);
+  });
+
+module.exports = app;
