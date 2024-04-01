e0d2a1d86eb534b681a2c44132596202c6a2ae28
leiocopio <107382632+leiocopio@users.noreply.github.com>
2024-04-01 10:21:47 +0800

    Added Google Sign In

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,18 @@ import "animate.css";
 import ReactDOM from "react-dom/client";
 import "./css/index.css";
 import App from "./App";
-import { AuthContextProvider } from './contexts/AuthContext'
+import { AuthContextProvider } from "./contexts/AuthContext";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+const GoogleClientID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+console.log("GoogleClientID:", GoogleClientID);
+
 root.render(
+  <GoogleOAuthProvider clientId={GoogleClientID}>
     <AuthContextProvider>
       <App />
     </AuthContextProvider>
+  </GoogleOAuthProvider>
 );
